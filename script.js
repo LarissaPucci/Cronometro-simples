086f8f1c3255faff68ad7289f34c7903e74db463
LarissaPucci <114083176+LarissaPucci@users.noreply.github.com>
2023-01-13 11:08:46 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,42 @@
+var sec = 0;
+var min = 0;
+var hr = 0;
+
+var time;
+
+function twoDigits(digits){
+    if(digits<10){
+        return ('0'+digits)
+    }else{
+        return digits
+    }
+}
+
+function start(){
+    counter()
+    time =setInterval(counter, 1);
+}
+
+function pause(){
+    clearInterval(time);
+}
+function zerar(){
+    clearInterval(time);
+    sec = 0;
+    min = 0;
+    hr = 0;
+    document.querySelector('.time').innerHTML = '00:00:00'
+}
+
+function counter(){
+    sec++
+    if(sec == 60){
+        min++;
+        sec = 0;
+        if(min == 60){
+            min=0;
+            hr++;
+        }
+    }
+    document.querySelector('.time').innerHTML =twoDigits(hr)+ ':'+twoDigits(min)+ ':'+twoDigits(sec);
+}
